730080d38c3d51ce75e7a8a12fa06056b4f6b7e9
Akki747 <bairapuakshaya@gmail.com>
2024-12-07 09:53:28 +0530

    Add files via upload

---
+++ classfor.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class classfor
+{
+public static void main(String args[])
+    {
+       Scanner Sc=new Scanner(System.in);
+       int a=Sc.nextInt();
+       int b=Sc.nextInt();
+       for(int i=0;i<=a;i++)
+    {
+    System.out.println("Akshaya");
+    }
+    for(int j=0;j<=b;j++)
+     {
+    System.out.println("Ajay");
+     }
+   }
+}
